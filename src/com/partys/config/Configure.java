0d59d9c81008b2a4e82c49f0969ffad672b3ae05
kunkka <jyf19881228@163.com>
2016-10-17 17:57:56 +0800

    123

--- src/com/partys/config/Configure.java
+++ src/com/partys/config/Configure.java
@@ -1,16 +1,14 @@
 package com.partys.config;
 
-import java.awt.BorderLayout;
 import java.awt.Color;
-import java.awt.Dimension;
-import java.awt.Graphics;
 
 import javax.swing.BorderFactory;
 import javax.swing.ImageIcon;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.border.EmptyBorder;
+import javax.swing.JPasswordField;
+import javax.swing.JTextField;
 
 import com.partys.tools.MyTools;
 
@@ -20,8 +18,10 @@ public class Configure extends JFrame {
 	 * 
 	 */
 	private static final long serialVersionUID = 6417159577663065198L;
-	private JLabel title,content,icon;
-	private JPanel p1;
+	private JLabel title,content,icon,username,code;
+	private JTextField user;
+	private JPasswordField pwd;
+	private JPanel p1,p2,p3;
 	
 
 	public static void main(String[] args) {
@@ -36,8 +36,7 @@ public class Configure extends JFrame {
 
 	private void ini() {
 		// TODO 自动生成的方法存根
-		title=new JLabel("用户信息");
-		
+		title=new JLabel("用户信息");		
 		title.setBounds(30, 10, 100, 30);
 		content=new JLabel("请输入您的注册信息，以便激活各项功能");
 		content.setFont(MyTools.f2);
@@ -51,6 +50,14 @@ public class Configure extends JFrame {
 		p1.setBounds(0, 0, 500, 80);
 		p1.setBackground(Color.white);
 		p1.setBorder(BorderFactory.createMatteBorder(0, 0, 1, 0, Color.DARK_GRAY));
+		
+		p2=new JPanel(null);
+		user=new JTextField();
+		pwd=new JPasswordField();
+		username=new JLabel();
+		code=new JLabel();
+		
+		
 		this.add(p1);
 		
 		this.setLayout(null);
