6c75dfec76948276a56004a8be4f1cd99fc8b2ba
kunkka <jyf19881228@163.com>
2016-11-15 17:38:30 +0800

    u

--- src/com/partys/login/loading.java
+++ src/com/partys/login/loading.java
@@ -1,5 +1,7 @@
-
 package com.partys.login;
+
+
+
 import java.awt.BorderLayout;
 import java.awt.Color;
 
@@ -8,9 +10,20 @@ import javax.swing.JLabel;
 import javax.swing.JProgressBar;
 import javax.swing.JWindow;
 
+import org.eclipse.jgit.api.errors.CanceledException;
+import org.eclipse.jgit.api.errors.DetachedHeadException;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidConfigurationException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.api.errors.RefNotAdvertisedException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
+
 import com.partys.tools.BasicUtil;
-import com.tools.UpdateFromGitHub;
-public class loading extends JWindow implements Runnable{
+import com.partys.tools.UpdateFromGitHub;
+
+public class loading extends JWindow{
 	/**
 	 * 
 	 */
@@ -18,58 +31,83 @@ public class loading extends JWindow implements Runnable{
 
 	private JProgressBar jpb;
 	private JLabel jl1;
-	public static void main(String []args){
-		loading index=new loading();
 
-		Thread t=new Thread(index);
-
-		t.start();
+	public static void main(String[] args) {
+		new loading();
 	}
 
-	public loading()
-	{	
+	public loading() {
 
-		jl1=new JLabel(new ImageIcon("image/index/index.png"));
-		
+		jl1 = new JLabel(new ImageIcon("image/index/index.png"));
 
-		jpb=new JProgressBar();
+		jpb = new JProgressBar();
 
 		jpb.setStringPainted(true);
 		jpb.setIndeterminate(false);
 		jpb.setBorderPainted(false);
 		jpb.setBackground(Color.pink);
-		try {
-			new UpdateFromGitHub().update(".", "https://github.com/kunkka1228/Management.git");
-		} catch (Exception e) {
-			// TODO 自动生成的 catch 块
-			e.printStackTrace();
-		}
-		this.add(jl1,BorderLayout.NORTH);
-		this.add(jpb,BorderLayout.SOUTH);
-		this.setSize(450,350);
+		jpb.setValue(0);
+		jpb.setString("检查更新中");
+		this.add(jl1, BorderLayout.NORTH);
+		this.add(jpb, BorderLayout.SOUTH);
+		this.setSize(450, 357);
 		this.setLocation(BasicUtil.getSreenWidthAndHeight()[0]/2-200,BasicUtil.getSreenWidthAndHeight()[1]/2-150);
-
 		this.setVisible(true);
-	}
+		UpdateFromGitHub ufg = new UpdateFromGitHub();
 
-	public void run() {
-
-		int []progressValue={0,1,5,8,14,17,26,35,38,43,49,56,65,71,75,78,86,94,98,99,100};
-		for(int i=0;i<progressValue.length;i++)
-		{
+		try {			
+			ufg.update(".", "https://github.com/kunkka1228/HongPaManagement.git");
 			try {
 
-				Thread.sleep(100);
+				Thread.sleep(1000);
 			} catch (InterruptedException e) {
 
 				e.printStackTrace();
 			}
-			jpb.setValue(progressValue[i]);
-			if(i==progressValue.length-1){
-				new UserLogin();
-
-					this.dispose();
-			}
+			jpb.setValue(100);
+			jpb.setBackground(Color.green);
+			jpb.setString("更新完成 ");
+		} 
+		 catch (WrongRepositoryStateException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (InvalidConfigurationException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (DetachedHeadException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (InvalidRemoteException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (CanceledException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (RefNotAdvertisedException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (NoHeadException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
+		} catch (TransportException e) {
+			// TODO 自动生成的 catch 块
+			jpb.setString("更新失败 ,无法连接服务器");
+			e.printStackTrace();
+			return;
+			
+		} catch (GitAPIException e) {
+			// TODO 自动生成的 catch 块
+			e.printStackTrace();
 		}
+		finally{
+			try {
+				Thread.sleep(2000);
+			} catch (InterruptedException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
+			new UserLogin();
+			this.dispose();
+		}	
 	}
 }
---
+++ src/com/partys/tools/UpdateFromGitHub.java
@@ -0,0 +1,48 @@
+package com.partys.tools;
+import java.io.File;
+import java.io.IOException;
+
+import org.eclipse.jgit.api.CheckoutCommand;
+import org.eclipse.jgit.api.CreateBranchCommand;
+import org.eclipse.jgit.api.Git;
+import org.eclipse.jgit.api.PullCommand;
+import org.eclipse.jgit.api.PullResult;
+import org.eclipse.jgit.api.errors.CanceledException;
+import org.eclipse.jgit.api.errors.DetachedHeadException;
+import org.eclipse.jgit.api.errors.GitAPIException;
+import org.eclipse.jgit.api.errors.InvalidConfigurationException;
+import org.eclipse.jgit.api.errors.InvalidRemoteException;
+import org.eclipse.jgit.api.errors.NoHeadException;
+import org.eclipse.jgit.api.errors.RefNotAdvertisedException;
+import org.eclipse.jgit.api.errors.TransportException;
+import org.eclipse.jgit.api.errors.WrongRepositoryStateException;
+
+public class UpdateFromGitHub{
+	
+
+	public void update(String local,String remote) throws WrongRepositoryStateException, InvalidConfigurationException, DetachedHeadException, InvalidRemoteException, CanceledException, RefNotAdvertisedException, NoHeadException, TransportException, GitAPIException{
+		
+		File file = new File(local);
+		File gitFile = new File(local+"/.git");
+		Git gitResult=null;
+		if (gitFile.exists()) {
+			try {
+				gitResult = Git.open(file);
+			} catch (IOException e) {
+				// TODO 自动生成的 catch 块
+				e.printStackTrace();
+			}
+			CheckoutCommand cc=gitResult.checkout();
+
+			cc.setCreateBranch(false);
+			cc.setName("master");
+			cc.setUpstreamMode(CreateBranchCommand.SetupUpstreamMode.TRACK);
+			
+			cc.call();		
+			PullCommand pullCmd=gitResult.pull();
+			pullCmd.call();
+
+		}
+	}
+	
+}
